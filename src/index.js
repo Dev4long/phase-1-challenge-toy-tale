39b3469a93137f246efefa10e58a3f3abc33c3a2
deniskadrich <dev4long@live.com>
2021-04-14 10:23:17 -0400

    partially done

--- src/index.js
+++ src/index.js
@@ -13,3 +13,30 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+fetch('http://localhost:3000/toys')
+.then(response => {response.json()})
+.then((toysArray) => {
+  toysArray.forEach(function(toyObject){
+  turnToyObjectToHtml(toyObject)
+  
+})
+})
+
+function turnToyObjectToHtml(toyPOJO) {
+  console.log(toyPOJO)
+  let outerLi = document.createElementById("li")
+    outerLi.className = "toy"
+
+  let toyNameP = document.createElement("p")
+    toyNameP.className = "js-toy middle alligned content"
+
+  let likesSpan = document.createElement("likes")
+    likesSpan.innerText = toyPOJO.Likes
+
+  let incrementButton = document.createElement("button")
+    incrementButton.innerText = "likes"
+
+  let toyImage = document.createElement('img')
+    toyImage.src = toyPOJO.image
+}
